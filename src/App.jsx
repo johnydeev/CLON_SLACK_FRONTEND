157be98ad0da21eeb3b1b3b5371420a11fc39e89
johnydeev <castrojonathand@gmail.com>
2025-04-03 09:29:57 -0300

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
                     <Route path="/rewrite-password" element={<RewritePasswordScree />} />
                     <Route path="/reset-password" element={<ResetPasswordScreen />} />
                 <Route element={<ProtectedRoute/>}>
-                    {/* <Route path="/" element={<HomeScreen />} /> */}
+                    <Route path="/" element={<RegisterScreen />} />
                     <Route path="/user/:user_id/workspaces" element={<WorkspacesScreen />} />
                     <Route path="/user/:user_id/workspaces/:workspace_id" element={<HomeScreen />} />
                     
